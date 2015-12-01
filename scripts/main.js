be0883d15a56550c0bd1857a6ef1f9510568d41d
Aaron Emery <aemery14@gmail.com>
2015-12-01 13:02:40 -0500

    Worked on top page. Currently working on projects section. Hover feature currently works on first project example

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+$('#js_aboutLink').click(function(){
+  $.scrollTo($('#about') , 800);
+  return false;
+});
+
+$('#js_projectsLink').click(function(){
+  $.scrollTo($('#projects') , 800);
+  return false;
+});
+
+$('#js_contactLink').click(function(){
+  $.scrollTo($('#contact') , 800);
+  return false;
+});
