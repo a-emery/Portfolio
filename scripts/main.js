8df43dc59973f847c83b194aea9962d074a914c4
Aaron Emery <aemery14@gmail.com>
2015-12-02 14:10:12 -0500

    Added to top arrow to nav. Reversed order of projects and about secions

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,40 @@
 $('#js_aboutLink').click(function(){
-  $.scrollTo($('#about') , 800, { offset:-75 });
+  $.scrollTo($('#about') , 800);
   return false;
 });
 
 $('#js_projectsLink').click(function(){
-  $.scrollTo($('#projects') , 800, { offset:-75 });
+  $.scrollTo($('#projects') , 800);
   return false;
 });
 
 $('#js_contactLink').click(function(){
-  $.scrollTo($('#contact') , 800, { offset:-75 });
+  $.scrollTo($('#contact') , 800);
   return false;
+});
+
+$('#js_topLink').click(function(){
+  $.scrollTo($('html'), 800);
+  return false;
+});
+
+$('.headerArrowContainer').hover(function() {
+  $('.headerArrow').css("display", "none");
+  $('.headerArrowHover').css("display", "block");
+}, function() {
+  $('.headerArrow').css("display", "block");
+  $('.headerArrowHover').css("display", "none");
+});
+
+var isVisible = false;
+
+$(window).scroll(function(){
+     var shouldBeVisible = $(window).scrollTop()>($('.heroContent').css('height').slice(0, -2) - 1);
+     if (shouldBeVisible && !isVisible) {
+          isVisible = true;
+          $('.headerArrowContainer').css("display", "inline-block");
+     } else if (isVisible && !shouldBeVisible) {
+          isVisible = false;
+          $('.headerArrowContainer').css("display", "none");
+    }
 });
